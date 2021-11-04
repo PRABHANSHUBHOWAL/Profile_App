12782a4198b8deaceab24d78ab722947cd32a12b
Prabhanshu Bhowal <prabhanshubhowal@gmail.com>
2021-11-04 15:52:35 +0530

    Initial commit

--- app/src/main/java/com/example/profileapp/MainActivity.java
+++ app/src/main/java/com/example/profileapp/MainActivity.java
@@ -10,9 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-=======
-
->>>>>>> 9c641b2... Initial commit
     }
 }
